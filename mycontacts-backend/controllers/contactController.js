1184fe6257e6cad8b4914cbbf8515e1fe3f99e9d
Hasan Faysal <hasanfaysal17@gmail.com>
2023-07-13 23:04:19 +0500

    changes

--- mycontacts-backend/controllers/contactController.js
+++ mycontacts-backend/controllers/contactController.js
@@ -21,7 +21,14 @@ const getContacts = asyncHandler(async (req, res) => {
 //@access public
 
 const getContact =asyncHandler( async (req,res)=>{
-    res.status(200).json({message:`Get contact for ${req.params.id}` });
+
+
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("Not Found")
+    }
+    res.status(200).json(contact);
 });
 
 //desc create contacts
@@ -38,14 +45,32 @@ const createContact =asyncHandler(async (req,res)=>{
 
     }
 
-    res.status(201).json({message:"create contact" });
+    const contact = await Contact.create({
+        name,
+        email,
+        phone,
+    });
+    res.status(201).json(contact);
 });
 
 const deleteContact =asyncHandler( async (req,res)=>{
     res.status(200).json({message:`delete contact for ${req.params.id}` });
 });
 const updateContact =asyncHandler( async (req,res)=>{
-    res.status(200).json({message:`update contact for ${req.params.id}` });
+    
+    
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("Not Found")
+    }
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new:true}
+        )
+    
+    res.status(200).json(updatedContact);
 });
 
 module.exports ={deleteContact, getContacts, createContact, getContact,updateContact };
